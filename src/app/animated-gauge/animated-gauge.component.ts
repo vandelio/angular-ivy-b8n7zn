1fb77e2d51328eb7f52dfa486553cbc58dc1f3c3
vandelio <stefanvandel@gmail.com>
2022-12-05 11:21:20 +0100

    3 files modified

--- src/app/animated-gauge/animated-gauge.component.ts
+++ src/app/animated-gauge/animated-gauge.component.ts
@@ -3,32 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'animated-gauge',
   templateUrl: './animated-gauge.component.html',
-  /*template: `
-      <div class="gauge">
-        <div class="skill">
-          <div class="outer">
-            <div class="inner">
-              <div data-value="65" id="number">65%</div>
-            </div>
-          </div>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.1"
-            width="160px"
-            height="160px"
-          >
-            <defs>
-              <linearGradient id="GradientColor">
-                <stop offset="0%" stop-color="#e91e63" />
-                <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
-          </svg>
-        </div>
-      </div>
-    `,*/
   styleUrls: ['./animated-gauge.component.scss'],
 })
 export class AnimatedGaugeComponent implements OnInit {
